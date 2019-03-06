31e133800fac445c7bf629e663173bde8c66f2b9
Paige Gorry <paigeegorry@gmail.com>
2019-03-06 14:27:05 -0800

    good luck

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import AllUsers from '../../containers/AllUsers';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
+import Users from '../users/Users';
 
 export default function App() {
   return (
     <>
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/users" component={Users} />
       </Switch>
     </Router>
       <h1>Redux Blog</h1>

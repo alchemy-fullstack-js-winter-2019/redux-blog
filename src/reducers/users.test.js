0ec538f0a76515562200c697c1d664f904e063e9
Paige Gorry <paigeegorry@gmail.com>
2019-03-07 11:44:33 -0800

    users reducers tests passing

--- src/reducers/users.test.js
+++ src/reducers/users.test.js
@@ -2,7 +2,7 @@ import reducer from './users';
 import { FETCH_POST_BY_USER_ID, FETCH_USER, FETCH_USERS } from '../action/users';
 
 describe('reducer test', () => {
-  it('can handle the fetch user action', () => {
+  it('can handle the fetch users from state', () => {
     const state = {
       users: {
         users: ['orange', 'banana', '420']
@@ -15,4 +15,27 @@ describe('reducer test', () => {
     expect(reducerUsers).toEqual({ users: ['orange', 'banana', '420'] });
   });
   
+  it('can fetch a user from state', () => {
+    const state = {
+      user: { 
+        name: 'yolo' 
+      }
+    };
+    const reducerUser = reducer(state, {
+      type: FETCH_USER,
+      payload: { name: 'yolo' }
+    });
+    expect(reducerUser).toEqual({ user: { name: 'yolo' } });
+  });
+
+  it('can handle the fetch posts by user id', () => {
+    const state = {
+      posts: ['orange', 'banana', '420']
+    };
+    const reducerUser = reducer(state, {
+      type: FETCH_POST_BY_USER_ID,
+      payload: ['orange', 'banana', '420']
+    });
+    expect(reducerUser).toEqual({ posts: ['orange', 'banana', '420'] });
+  });
 });

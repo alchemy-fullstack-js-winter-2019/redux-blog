f3a184fae366111fbe590592c6ba90eb2b5a7e3c
cruzayye <johnnyc010392@gmail.com>
2019-03-08 10:19:15 -0800

    few tests passing

--- src/selectors/posts.test.js
+++ src/selectors/posts.test.js
@@ -1,4 +1,5 @@
-import { getPostsBySearch } from './posts';
+import { getPostsBySearch, getPosts, getUser, getAllUsers, getPost } from './posts';
+
 
 
 describe('test', () => {
@@ -11,8 +12,45 @@ describe('test', () => {
         searchTerm: 's'
       }
     }; 
-
     const results = getPostsBySearch(state);
     expect(results).toHaveLength(1);
+  });
+  it('gets all posts', () => {
+    const state = {
+      posts: {
+        posts: [{ whatever: 'something something something' }]
+      }
+    };
+    const result = getPosts(state);
+    expect(result).toHaveLength(1);
+  });
+  it('gets 1 only one user', () => {
+    const state = {
+      posts: {
+        user: { user1: 'someUser' }
+      }
+    };
+    const result = getUser(state);
+    expect(result.user1).toEqual('someUser');  
+  });
+
+  it('getAllUsers', () => {
+    const state = {
+      users: {
+        users: [{ user: 'user' }, { user2: 'another1' }]
+      }
+    };
+    const result = getAllUsers(state);
+    expect(result).toHaveLength(2);
+  });
+
+  it('gets a post', () => {
+    const state = {
+      posts: {
+        post: { post: 'post1' }
+      }
+    };
+    const result = getPost(state);
+    expect(result).toEqual({ post: 'post1' });
   });
 });

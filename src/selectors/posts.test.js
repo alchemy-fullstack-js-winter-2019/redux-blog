26d8155f379712aef36b419203184fe2cef3bbde
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-03-08 10:33:38 -0800

    posts selectors all pass tests

--- src/selectors/posts.test.js
+++ src/selectors/posts.test.js
@@ -1,9 +1,7 @@
-import { getPostsBySearch, getPosts, getUser, getAllUsers, getPost } from './posts';
-
-
+import { getPostsBySearch, getPosts, getUser, getAllUsers, getPost, getSearchTerm, getCommentsById, getPostsById, getLongestPost } from './posts';
 
 describe('test', () => {
-  it('returns an array of posts', () => {
+  it('returns a filtered array of posts', () => {
     const state = {
       posts: {
         posts: [
@@ -52,5 +50,66 @@ describe('test', () => {
     };
     const result = getPost(state);
     expect(result).toEqual({ post: 'post1' });
+  });
+  it('can return the currect search term', () => {
+    const state = {
+      posts: {
+        posts: [
+          { title: 'sometitle' }
+        ],
+        searchTerm: 's'
+      }
+    }; 
+
+    const result = getSearchTerm(state);
+    expect(result).toEqual('s');
+  });
+
+  it('can return an array of comments for a specific post', () => {
+    const state = {
+      posts: {
+        comments: [
+          { comment: 'some comment' },
+          { comment: 'some comment' },
+          { comment: 'some comment' }
+        ],
+        searchTerm: 's'
+      }
+    }; 
+    const result = getCommentsById(state);
+    expect(result).toHaveLength(3);
+
+  });
+  it('can return a single user`s posts by id', () => {
+    const state = {
+      users: {
+        postsByUser: [
+          { post: 'some post' },
+          { post: 'some post' },
+          { post: 'some post' }
+        ],
+      }
+    }; 
+
+    const result = getPostsById(state);
+    expect(result).toHaveLength(3);
+  });
+
+  it('can return the longest posts from an array of posts', () => {
+    const state = {
+      posts: {
+        posts: [
+          { body: 'some post' },
+          { body: 'some post2' },
+          { body: 'some post123' },
+          { body: 'some post1214' },
+          { body: 'some post1225' },
+          { body: 'some pos121216' },
+          { body: 'some post121217' }
+        ],
+      }
+    }; 
+    const result = getLongestPost(state);
+    expect(result).toHaveLength(5);
   });
 });

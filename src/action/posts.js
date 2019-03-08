392d57e6c2d83d5e167e453079b002e7a9afabdc
Paige Gorry <paigeegorry@gmail.com>
2019-03-07 17:24:31 -0800

    loading start and end for posts action tests passing

--- src/action/posts.js
+++ src/action/posts.js
@@ -1,20 +1,31 @@
 import { getPosts, getPost } from '../services/blogApi';
 
-
+export const LOAD_POSTS_START = 'LOAD_POSTS_START';
+export const LOAD_POSTS_END = 'LOAD_POSTS_END';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const fetchPosts = () => ({
   type: FETCH_POSTS,
-  payload: getPosts()
+  loadStart: LOAD_POSTS_START,
+  payload: getPosts(),
+  loadEnd: LOAD_POSTS_END
 });
 
+export const LOAD_POST_START = 'LOAD_POST_START';
+export const LOAD_POST_END = 'LOAD_POST_END';
 export const FETCH_POST = 'FETCH_POST';
 export const fetchPost = id => ({
   type: FETCH_POST,
-  payload: getPost(id)
+  loadStart: LOAD_POST_START,
+  payload: getPost(id),
+  loadEnd: LOAD_POST_END
 });
 
+export const UPDATE_POST_SEARCH_START = 'UPDATE_POST_SEARCH_START';
+export const UPDATE_POST_SEARCH_END = 'UPDATE_POST_SEARCH_END';
 export const UPDATE_POST_SEARCH_TERM = 'UPDATE_POST_SEARCH_TERM';
 export const updatePostSearchTerm = term => ({
   type: UPDATE_POST_SEARCH_TERM,
-  payload: term
+  loadStart: UPDATE_POST_SEARCH_START,
+  payload: term,
+  loadEnd: UPDATE_POST_SEARCH_END
 }); 
--- src/action/posts.test.js
+++ src/action/posts.test.js
@@ -1,4 +1,14 @@
-import { fetchPost, fetchPosts, updatePostSearchTerm } from './posts';
+import { 
+  fetchPost, 
+  fetchPosts, 
+  updatePostSearchTerm,
+  LOAD_POSTS_END,
+  LOAD_POSTS_START,
+  LOAD_POST_START,
+  LOAD_POST_END,
+  UPDATE_POST_SEARCH_START,
+  UPDATE_POST_SEARCH_END
+} from './posts';
 
 jest.mock('../services/blogApi');
 
@@ -7,13 +17,15 @@ describe('posts actions', () => {
     const fetchedPost = fetchPost();
     expect(fetchedPost).toEqual({
       type: 'FETCH_POST',
+      loadStart: LOAD_POST_START,
       payload: Promise.resolve(
         {
           'userId': 1,
           'id': 1,
           'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
           'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
-        })
+        }),
+      loadEnd: LOAD_POST_END
     });
   });
 
@@ -21,6 +33,7 @@ describe('posts actions', () => {
     const fetchedPosts = fetchPosts();
     expect(fetchedPosts).toEqual({
       type: 'FETCH_POSTS',
+      loadStart: LOAD_POSTS_START,
       payload: Promise.resolve(
         {
           'userId': 1,
@@ -34,14 +47,17 @@ describe('posts actions', () => {
           'title': 'qui est esse',
           'body': 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'
         }
-      )
+      ),
+      loadEnd: LOAD_POSTS_END
     });
   });
   it('can update a search term', () => {
     const updatedPostSearchTerm = updatePostSearchTerm('term');
     expect(updatedPostSearchTerm).toEqual({
       type: 'UPDATE_POST_SEARCH_TERM',
-      payload: 'term'
+      loadStart: UPDATE_POST_SEARCH_START,
+      payload: 'term',
+      loadEnd: UPDATE_POST_SEARCH_END
     });
   });
 });

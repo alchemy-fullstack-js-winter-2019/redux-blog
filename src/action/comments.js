993ecb3fd8cfdf57397cc0e80461fd0d2065ecbf
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-11 15:07:32 -0700

    fixed eslint and warning on start

--- src/action/comments.js
+++ src/action/comments.js
@@ -3,7 +3,8 @@ import { createAction } from 'promise-middleware-redux';
 
 export const [ 
   fetchComments, 
-  FETCH_COMMENTS, 
+  FETCH_COMMENTS,
+  FETCH_COMMENTS_LOADING,
   LOAD_COMMENTS_START, 
   LOAD_COMMENTS_END 
 ] = createAction('FETCH_COMMENTS', getComments);
